e547b8496dfb84956a50965f3cdb353205655166
saminyead <Saminyead@gmail.com>
2024-03-03 16:59:11 +0600

    test added test_adding_to_file_add_to_gpu_units_of_interest_file to check if gpu units are being properly added

--- old_structure/new_gpu_unit/test_new_gpu_unit.py
+++ old_structure/new_gpu_unit/test_new_gpu_unit.py
@@ -5,6 +5,7 @@ from exceptions import GPUAlreadyExistsError, InvalidGpuUnitFormatError
 
 TEST_DATA_DIR:str = "./test_data_dir"
 EXISTS_FILE:str = "exists.txt"
+DOES_NOT_EXIST_FILE:str = "not_exist.txt"
 
 GPU_UNITS_EXISTING:list[str] = [
     "RX 6950 XT",
@@ -33,7 +34,7 @@ def _create_gpu_units_of_interest_test_file(
     return filedir
 
 
-def test_check_does_not_add_to_existing_add_to_gpu_units_of_interest_file(
+def test_does_not_add_to_existing_add_to_gpu_units_of_interest_file(
         gpu_unit_of_interest_list:list[str] = GPU_UNITS_EXISTING,
 ) -> None:
     """Checks if add_unit_to_gpu_units_of_interests_file checks if
@@ -42,8 +43,31 @@ def test_check_does_not_add_to_existing_add_to_gpu_units_of_interest_file(
 
     for gpu_unit in gpu_unit_of_interest_list:
         with pytest.raises(GPUAlreadyExistsError):
-            add_to_gpu_units_of_interest_file(full_filename=filedir,gpu_unit=gpu_unit)
+            add_to_gpu_units_of_interest_file(
+                full_filename=filedir,
+                gpu_unit=gpu_unit
+            )
 
+
+def test_adding_to_file_add_to_gpu_units_of_interest_file(
+        filename:str = f"{TEST_DATA_DIR}/{DOES_NOT_EXIST_FILE}",
+        gpu_units_of_interest_list:list[str] = GPU_UNITS_NOT_EXISTING
+) -> None:
+    """Checks if gpu unit names are being properly added to
+    the gpu_units_of_interest file"""
+    _create_gpu_units_of_interest_test_file(
+        filedir=filename,
+        gpu_units_of_interest_list=[]
+    )
+
+    for gpu_unit in gpu_units_of_interest_list:
+        add_to_gpu_units_of_interest_file(gpu_unit,filename)
+        
+    with open(filename,'r') as gpu_units_reader:
+        gpu_units = gpu_units_reader.read()
+        gpu_units_list = gpu_units.splitlines()
+
+    assert gpu_units_of_interest_list == gpu_units_list
 
 
 # This should be for a different function, not 
@@ -51,6 +75,9 @@ def test_check_does_not_add_to_existing_add_to_gpu_units_of_interest_file(
 # input function you will use in main
 def test_input_format_is_valid_input_gpu_unit_name() -> None:
     """Checks if the format of the gpu_unit_name input is valid"""
+
+    with pytest.raises(InvalidGpuUnitFormatError):
+        input_gpu_unit_name("Not even a GPU name")
     
     with pytest.raises(InvalidGpuUnitFormatError):
         input_gpu_unit_name("GeForce RTX 3060")
