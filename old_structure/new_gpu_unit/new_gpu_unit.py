dd19660da9e6b9ab459b71910edd3b070020970f
saminyead <Saminyead@gmail.com>
2024-03-03 16:16:20 +0600

    new exception added InvalidGpuUnitFormatError to check if the gpu_unit_name is being properly input

--- old_structure/new_gpu_unit/new_gpu_unit.py
+++ old_structure/new_gpu_unit/new_gpu_unit.py
@@ -13,11 +13,25 @@ GEFORCE_GPU_UNITS_OF_INTEREST_FILE = "geforce_gpu_units.txt"
 
 
 class GPUAlreadyExistsError(Exception):
-    def __init__(self, gpu_name:str, filename:str):
-        self.gpu_name = gpu_name
+    def __init__(self, gpu_unit_name:str, filename:str):
+        self.gpu_unit_name = gpu_unit_name
         self.filename = filename
-        super().__init__(f"{gpu_name} already exists in file {filename}")
+        super().__init__(f"{gpu_unit_name} already exists in file {filename}")
 
+
+class InvalidGpuUnitFormatError(Exception):
+    def __init__(self, gpu_unit_name:str) -> None:
+        super().__init__(f"""The name {gpu_unit_name} is not a valid 
+                         GPU unit name. Please use the following format:
+                         - 'RTX ****'/'GTX ****' for GeForce GPUs
+                         - 'RX ****' for Radeon GPUs
+                         - 'Arc ****' for Arc GPUs
+                         If there has been a new release lineups, then please
+                         change the _check_gpu_unit_name_valid_input function
+                         in the new_gpu_unit module""")
+
+# TODO: define another exception to be raised if gpu_unit_name does not
+        # start with RTX/RX and so on
 
 def _check_if_exists_gpu_units_of_interest_file(
         gpu_unit:str,
@@ -40,6 +54,10 @@ def add_to_gpu_units_of_interest_file(
         full_filename:str,
 ) -> None:
     """Adds a new gpu unit to the appropriate file"""
+    # TODO: - tie the filename to the gpu_unit 
+    #       (e.g. if RTX|GTX then geforce file etc.)
+    #       - add test to see if this is being ensured
+    # 
     if _check_if_exists_gpu_units_of_interest_file(gpu_unit,full_filename):
         raise GPUAlreadyExistsError(gpu_unit,full_filename)
     
