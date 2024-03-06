41b05d5446d6b943ec0e8c6b6d1fb57e2dcc5726
saminyead <Saminyead@gmail.com>
2024-03-06 19:48:19 +0600

    changed delete function to replace_previous_data function

--- database.py
+++ database.py
@@ -6,26 +6,26 @@ from datetime import date
 from logger import setup_logging
 
 def push_to_db(
-        db_url:str,
+        conn:sqlalchemy.engine.base.Connection,
         **df_kwargs:pd.DataFrame
 ) -> None:
     """Pushes dataframe to database
 
     Args:
-        db_url (str): the database connection url
+        conn (sqlalchemy.engine.base.Connection): the sqlalchemy 
+        connection object
         
         **df_kwargs (pd.DataFrame): a dictionary of dataframes as 
         keyword arguments
     """
     logging = setup_logging()
 
-    pgsql_db_engine = sqlalchemy.create_engine(db_url).connect()
     logging.info(msg='Connection to database established')
 
     for table_name,df in df_kwargs.items():
         df.to_sql(
             name=table_name,
-            con=pgsql_db_engine,
+            con=conn,
             if_exists='append',
             index=False
         )
@@ -36,23 +36,37 @@ def push_to_db(
 
 
 
-def delete_table_from_db(
+def replace_previous_date_data_table_db(
         conn:sqlalchemy.engine.base.Connection,
+        **df_kwargs:pd.DataFrame
 ) -> None:
-    logging = setup_logging()
-    # deleting all rows written before today
-    today = date.today().strftime('%Y-%m-%d')
-    metadata = sqlalchemy.MetaData()
-    lowest_prices_tiered_sqlalchemy = sqlalchemy.Table(
-        "lowest_prices_tiered",
-        metadata,
-        sqlalchemy.Column('retail_url',sqlalchemy.String,primary_key=True),
-        sqlalchemy.Column('data_collection_date',sqlalchemy.String)
-    )
-    
-    delete_previous_date_data = lowest_prices_tiered_sqlalchemy.delete().\
-        where(lowest_prices_tiered_sqlalchemy.c.data_collection_date!=today)
-    conn.execute(delete_previous_date_data)
-    logging.info(msg= 'Data of previous dates deleted.')
+    """Replace previous date's data with today's data (esp. for 
+    lowest_prices_tiered_sqlalchemy)
 
-    logging.info(msg=f'All dataframes written to database for {today}\n')
+    Args:
+        conn (sqlalchemy.engine.base.Connection): the sqlalchemy 
+        connection object
+
+        **df_kwargs (pd.DataFrame): dataframes as keyword arguments (
+        so that dataframe data can be pushed to table)
+    """
+    logging = setup_logging()
+    push_to_db(conn,**df_kwargs)
+
+    for table_name, df in df_kwargs:
+        # deleting all rows written before today
+        today = date.today().strftime('%Y-%m-%d')
+        metadata = sqlalchemy.MetaData()
+        db_table = sqlalchemy.Table(
+            table_name,
+            metadata,
+            sqlalchemy.Column('retail_url',sqlalchemy.String,primary_key=True),
+            sqlalchemy.Column('data_collection_date',sqlalchemy.String)
+        )
+        
+        delete_previous_date_data = db_table.delete().\
+            where(db_table.c.data_collection_date!=today)
+        conn.execute(delete_previous_date_data)
+        logging.info(msg= 'Data of previous dates deleted.')
+
+        logging.info(msg=f'All dataframes written to database for {today}\n')
