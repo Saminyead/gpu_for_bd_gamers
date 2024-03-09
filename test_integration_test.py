9b20c6ab58e252ccbd71ebfe35713227b80b37f4
saminyead <Saminyead@gmail.com>
2024-03-09 11:19:25 +0600

    changed the fixture to be a normal function that return a number of 'constant' values to be used throughout the test - I don't want this function to run every time before the test, since it takes WAY TOO LONG

--- test_integration_test.py
+++ test_integration_test.py
@@ -13,12 +13,15 @@ import sqlalchemy
 dotenv.load_dotenv("test.env")
 test_db_url = os.getenv("test_db_url")
 
-def sql_query_format(table_name:str):
-    """formats the sql query so we can just plug in table_name 
-    in test_main"""
-    today = datetime.datetime.today().strftime("%Y-%m-%d")
-    return f"SELECT * FROM {table_name} WHERE data_collection_date = '{today}'"
 
+
+def df_dict_test(
+) -> dict[str,pd.DataFrame]:
+    """Fixture for getting the dictionary of dataframes to
+    either push to database, or append them"""
+    # the data_collection_to_db needs to be tested in the future
+    # fixture will need to change then
+    return data_collection_to_df()
 
 # TODO:
 #    - create two database sets of tables for each of the dataframes
@@ -28,30 +31,29 @@ def sql_query_format(table_name:str):
 #    - one for pre-existing data
 #       - should raise error when trying to push
 
-@pytest.fixture
-def test_df_dict(
-) -> dict[str,pd.DataFrame]:
-    """Fixture for getting the dictionary of dataframes to
-    either push to database, or append them"""
-    # the data_collection_to_db needs to be tested in the future
-    # fixture will need to change then
-    return data_collection_to_df()
+TEST_DF_DICT = df_dict_test()
+TEST_DF_DICT_TO_APPEND = {
+    "gpu_of_interest": TEST_DF_DICT['gpu_of_interest'],
+    "lowest_prices" : TEST_DF_DICT['lowest_prices'],
+}
+TEST_DF_DICT_TO_REPLACE = {
+    "lowest_prices_tiered": TEST_DF_DICT['lowest_prices_tiered']
+}
+
+
+def sql_query_format(table_name:str):
+    """formats the sql query so we can just plug in table_name 
+    in test_main"""
+    today = datetime.datetime.today().strftime("%Y-%m-%d")
+    return f"SELECT * FROM {table_name} WHERE data_collection_date = '{today}'"
 
 
 def test_main(
-        test_df_dict:dict[str,pd.DataFrame],
+        test_df_dict_to_append:dict[str,pd.DataFrame]=TEST_DF_DICT_TO_APPEND,
+        test_df_dict_to_replace:dict[str,pd.DataFrame]=TEST_DF_DICT_TO_REPLACE,
         test_db_url:str = test_db_url,
     ):
     """Directly tests main on a test database"""
-    
-    test_df_dict_to_append = {
-        "gpu_of_interest": test_df_dict['gpu_of_interest'],
-        "lowest_prices" : test_df_dict['lowest_prices'],
-    }
-
-    test_df_dict_to_replace = {
-        "lowest_prices_tiered": test_df_dict['lowest_prices_tiered']
-    }
 
     data_collection_to_db(
         test_db_url,
