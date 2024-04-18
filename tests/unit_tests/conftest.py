8187d9bbb4464ed57ec1028ae32b6f42dbbb87e3
saminyead <Saminyead@gmail.com>
2024-04-18 12:10:20 +0600

    gpu_unit_index changed to contain manufacturer brand names as key

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -28,19 +28,25 @@ def master_df_test(filename:str=MASTER_DF_CSV_FILE) -> pd.DataFrame:
     return pd.read_csv(filename)
 
 @pytest.fixture
-def gpu_unit_index() -> dict[str,list[int]]:
+def gpu_unit_index() -> dict[str, dict[str, list[int]]]:
     return {
-        "Radeon RX 580": [0],
-        "Radeon RX 7600": [1],
-        "Radeon RX 7600 XT": [2],
-        "Radeon RX 6900 XT": [3,4],
-        "Geforce GTX 1650": [5,6],
-        "Geforce RTX 2060": [7],
-        "Geforce RTX 2080 Super": [8],
-        "Geforce RTX 4060": [9,10],
-        "Geforce RTX 4060 Ti": [11,12],
-        "Geforce RTX 3070": [13],
-        "Intel Arc A580": [14],
-        "Intel Arc A750": [15,16],
-        "Intel Arc A770": [17],
+        "Radeon": {
+            "Radeon RX 580": [0],
+            "Radeon RX 7600": [1],
+            "Radeon RX 7600 XT": [2],
+            "Radeon RX 6900 XT": [3,4],
+        },
+        "Geforce":{
+            "Geforce GTX 1650": [5,6],
+            "Geforce RTX 2060": [7],
+            "Geforce RTX 2080 Super": [8],
+            "Geforce RTX 4060": [9,10],
+            "Geforce RTX 4060 Ti": [11,12],
+            "Geforce RTX 3070": [13],
+        },
+        "Intel":{
+            "Intel Arc A580": [14],
+            "Intel Arc A750": [15,16],
+            "Intel Arc A770": [17],
+        },
     }
