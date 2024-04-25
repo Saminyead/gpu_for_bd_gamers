3ef7cb016c142bd86be4503956f6fb02579d2457
saminyead <Saminyead@gmail.com>
2024-04-25 10:56:37 +0600

    moved add_gpu_unit function to separate module

--- gpu4bdgamers/data_coll_script.py
+++ gpu4bdgamers/data_coll_script.py
@@ -17,6 +17,7 @@ import math
 from gpu4bdgamers.logger import setup_logging
 from gpu4bdgamers.overall_tier_score import df_overall_tier_score
 from gpu4bdgamers.database import push_to_db, replace_previous_date_data_table_db
+from gpu4bdgamers.gpu_units import add_gpu_unit_name
 
 
 def read_gpu_from_files(filename:str) -> list[str]:
@@ -285,27 +286,6 @@ def data_collection_to_df() -> dict[str,pd.DataFrame]:
 
     logging.info(msg=f'Total number of GPUs = {len(geforce_gpu_unit_list+radeon_gpu_unit_list+intel_gpu_unit_list)}')
 
-    # creating dataframes with GPU unit names in each row
-    def add_gpu_unit_name(target_df,gpu_list,gpu_brand):
-        """creates a dataframe from a larger dataframe (intended to be created from master_df), and add GPU unit names to each row
-        Args:
-            target_df (pandas DataFrame): the dataframe (mainly master_df) from which smaller dataframes will be extracted
-            gpu_list (list): list of GPU unit names, smaller dataframe from target_df will be extracted corresponding to each list entry
-            gpu_brand (string): the brand name of the GPU (Geforce/Intel Arc/Radeon)
-        Returns:
-            pandas.DataFrame: dataframe containing the graphics cards corresponding to each GPU in gpu_of_interest
-        """
-        gpu_base_name_df = pd.DataFrame()
-        for gpu in gpu_list:
-            df_with_gpu_name = target_df.loc[target_df['gpu_name'].str.contains(re.compile(gpu,flags=re.I))]
-            # in some websites, there are no spaces within the gpu names (e.g. RTX3080)
-            # thus bringing all the rest of the names into a common format with no space within gpu names
-            gpu_nospace = gpu.replace(' ','')
-            df_with_gpu_name_nospace = target_df.loc[target_df['gpu_name'].str.contains(re.compile(gpu_nospace,flags=re.I))]
-            df_with_gpu_name['gpu_unit_name'] = gpu_brand + ' ' +gpu
-            gpu_base_name_df = pd.concat([gpu_base_name_df,df_with_gpu_name,df_with_gpu_name_nospace])
-            gpu_base_name_df.drop_duplicates(subset='retail_url',keep='last',inplace=True)
-        return gpu_base_name_df
 
     # all geforce gpu's
     geforce_gpu_df = add_gpu_unit_name(master_df,geforce_gpu_unit_list,'Geforce')
