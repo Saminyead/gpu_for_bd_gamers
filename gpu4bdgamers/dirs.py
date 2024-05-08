7ccd57d7b83fdce118f9384d8e0e53d289795248
saminyead <Saminyead@gmail.com>
2024-05-08 17:24:57 +0600

    path formatting in the way shown in pahtlib documentation instead of string formatting

--- gpu4bdgamers/dirs.py
+++ gpu4bdgamers/dirs.py
@@ -1,6 +1,6 @@
-import os
+import pathlib
 
-PROJECT_ROOT = os.path.abspath('.')
-CONFIGS_DIR = f"{PROJECT_ROOT}/configs"
+PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
 
-SCRAPING_CONFIG_FILE = f"{CONFIGS_DIR}/scraping_config.toml"
+CONFIGS_DIR = PROJECT_ROOT/'configs'
+SCRAPING_CONFIG_FILE = CONFIGS_DIR/'scraping_config.toml'
