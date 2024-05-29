39706033ed1282d775ae0ae1a0f0b75b9836f9a5
saminyead <Saminyead@gmail.com>
2024-05-29 13:11:34 +0600

    set up logging properly read from argument instead of global variable

--- gpu4bdgamers/logger.py
+++ gpu4bdgamers/logger.py
@@ -1,12 +1,14 @@
 import logging
 from gpu4bdgamers.dirs import LOGS_DIR
 
-def setup_logging(log_dir = LOGS_DIR) -> logging.RootLogger:
+import pathlib
+
+def setup_logging(log_dir:pathlib.Path = LOGS_DIR) -> logging.RootLogger:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[
-            logging.FileHandler(LOGS_DIR/"gpu_data_coll_script.log",mode="a+"),
+            logging.FileHandler(log_dir/"gpu_data_coll_script.log",mode="a+"),
             logging.StreamHandler()
         ]
     )
