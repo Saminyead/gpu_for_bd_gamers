2d9d58d3b791041a6cda2046ddaf95e9a8c4b735
saminyead <Saminyead@gmail.com>
2024-05-29 13:26:58 +0600

    added gpu unit file directories to dirs module

--- gpu4bdgamers/dirs.py
+++ gpu4bdgamers/dirs.py
@@ -7,3 +7,8 @@ CONFIGS_DIR = PROJECT_ROOT/'configs'
 SCRAPING_CONFIG_FILE = CONFIGS_DIR/'scraping_config.toml'
 
 LOGS_DIR = PROJECT_ROOT/'logs'
+
+GPU_UNITS_LIST_DIR = PROJECT_ROOT/'gpu_units_of_interest'
+GEFORCE_UNITS_FILE = GPU_UNITS_LIST_DIR/'geforce_gpu_units.txt'
+RADEON_UNITS_FILE = GPU_UNITS_LIST_DIR/'radeon_gpu_units.txt'
+INTEL_UNITS_FILE = GPU_UNITS_LIST_DIR/'intel_gpu_units.txt'
