df5ba1ded9f5ec3185448263cca2b30ef3e3dc00
saminyead <Saminyead@gmail.com>
2024-05-29 13:48:08 +0600

    according to the new implementation of data_collection_to_df, the gpu units of interest files have been added as arguments

--- tests/integration_test/conftest.py
+++ tests/integration_test/conftest.py
@@ -15,8 +15,17 @@ with open(CURRENT_DIR/'scraping_config.toml','r') as f:
 FIRST_PAGE_URLS = scraping_config_contents['first_page_urls']
 CARD_CSS_SELECTORS = scraping_config_contents['card_css_selectors']
 
+RADEON_GPU_LIST_FILE_TEST = CURRENT_DIR/'gpu_units_of_interest'/'geforce_gpu_units.txt'
+GEFORCE_GPU_LIST_FILE_TEST = CURRENT_DIR/'gpu_units_of_interest'/'radeon_gpu_units.txt'
+INTEL_GPU_LIST_FILE_TEST = CURRENT_DIR/'gpu_units_of_interest'/'intel_gpu_units.txt'
+
 master_df:pd.DataFrame = get_master_df(FIRST_PAGE_URLS,CARD_CSS_SELECTORS)
-DF_DICT_TEST = data_collection_to_df(master_df)
+DF_DICT_TEST = data_collection_to_df(
+    master_df, 
+    RADEON_GPU_LIST_FILE_TEST, 
+    GEFORCE_GPU_LIST_FILE_TEST, 
+    INTEL_GPU_LIST_FILE_TEST
+)
 
 @pytest.fixture
 def df_dict_test(df_dict:dict=DF_DICT_TEST):
