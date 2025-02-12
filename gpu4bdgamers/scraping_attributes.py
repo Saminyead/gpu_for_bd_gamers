a48b93f8f560ebc86b8e8fef80e2d8357273b9af
saminyead <Saminyead@gmail.com>
2025-02-12 19:13:17 +0600

    internal method for iterating through the pages and return list of beautifulsoup objects

--- gpu4bdgamers/scraping_attributes.py
+++ gpu4bdgamers/scraping_attributes.py
@@ -28,6 +28,25 @@ class ScrapingAttributes:
         soup = BeautifulSoup(page_content,features='html.parser')
         return soup
     
+    def _find_all(self) -> list[BeautifulSoup]:
+        """Use the find_all() BeautifulSoup method to get a list of soup
+        objects from the first page url"""
+        next_page_url = self.first_page_url
+        soup_list: list[BeautifulSoup] = []
+        while True:
+            current_page_soup = self._get_page_soup(next_page_url)
+            soup_list.append(current_page_soup)
+            # part from here 
+            next_page_url_result_set = current_page_soup.find_all(
+                name='a',string=self.next_page_url_tag_str
+            )
+            if not next_page_url_result_set:
+                break
+            next_page_url = next_page_url_result_set[0]['href']
+            # to here should be a different function
+        return soup_list
+
+    
     def get_pages(self) -> list[BeautifulSoup]:
         """Starts with the first page url, and then scrapes all pages until it
         no longer finds a next page. Returns a list of pages as a list of 
