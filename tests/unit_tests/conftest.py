c55537a13606cb09bec5e9bc29fdd51c22fd9051
saminyead <Saminyead@gmail.com>
2025-04-27 12:11:07 +0600

    no matching element test fixture added

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,31 +1,38 @@
 import pytest
 import pandas as pd
 
+import subprocess
+from typing import Generator
 
 MASTER_DF_CSV_FILE = "master_df.csv"
+
 
 @pytest.fixture
 def radeon_gpu_list() -> list[str]:
     return ["RX 580", "RX 6900 XT", "RX 7600", "RX 7600 XT"]
 
+
 @pytest.fixture
 def geforce_gpu_list() -> list[str]:
     return [
-        "GTX 1650", 
-        "RTX 2060", 
-        "RTX 2080 Super", 
-        "RTX 3070", 
+        "GTX 1650",
+        "RTX 2060",
+        "RTX 2080 Super",
+        "RTX 3070",
         "RTX 4060",
-        "RTX 4060 Ti"
+        "RTX 4060 Ti",
     ]
+
 
 @pytest.fixture
 def intel_gpu_list() -> list[str]:
     return ["Arc A580", "Arc A750", "Arc A770"]
 
+
 @pytest.fixture
-def master_df_test(filename:str=MASTER_DF_CSV_FILE) -> pd.DataFrame:
+def master_df_test(filename: str = MASTER_DF_CSV_FILE) -> pd.DataFrame:
     return pd.read_csv(filename)
+
 
 @pytest.fixture
 def gpu_unit_index() -> dict[str, dict[str, list[int]]]:
@@ -34,19 +41,33 @@ def gpu_unit_index() -> dict[str, dict[str, list[int]]]:
             "Radeon RX 580": [0],
             "Radeon RX 7600": [1],
             "Radeon RX 7600 XT": [2],
-            "Radeon RX 6900 XT": [3,4],
+            "Radeon RX 6900 XT": [3, 4],
         },
-        "Geforce":{
-            "Geforce GTX 1650": [5,6],
+        "Geforce": {
+            "Geforce GTX 1650": [5, 6],
             "Geforce RTX 2060": [7],
             "Geforce RTX 2080 Super": [8],
-            "Geforce RTX 4060": [9,10],
-            "Geforce RTX 4060 Ti": [11,12],
+            "Geforce RTX 4060": [9, 10],
+            "Geforce RTX 4060 Ti": [11, 12],
             "Geforce RTX 3070": [13],
         },
-        "Intel":{
+        "Intel": {
             "Intel Arc A580": [14],
-            "Intel Arc A750": [15,16],
+            "Intel Arc A750": [15, 16],
             "Intel Arc A770": [17],
         },
     }
+
+
+@pytest.fixture(scope="module")
+def mock_server(html_pages_dir: str = "./mock_pages/", port="5000") -> Generator[None]:
+    proc = subprocess.Popen(
+        ["python3", "-m", "http.server", "-d", html_pages_dir, port]
+    )
+    yield
+    proc.kill()
+
+
+@pytest.fixture(scope="function")
+def no_next_page() -> str:
+    return "http://127.0.0.1:5000/no_next_page.html"
