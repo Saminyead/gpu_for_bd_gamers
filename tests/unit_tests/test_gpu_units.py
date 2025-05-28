e8be0c37901c4af47a4cbe43ffe4d7e7934a4567
saminyead <Saminyead@gmail.com>
2025-05-28 10:33:55 +0600

    added test to check if GTX 1050 Ti can be added without making a separate function for it yes it can

--- tests/unit_tests/test_gpu_units.py
+++ tests/unit_tests/test_gpu_units.py
@@ -5,9 +5,8 @@ import pandas as pd
 
 FILE_FOR_TEST = "gpu_units_test.txt"
 
-def test_no_empty_str_read_gpu_from_files(
-    filename:str = FILE_FOR_TEST
-) -> None:
+
+def test_no_empty_str_read_gpu_from_files(filename: str = FILE_FOR_TEST) -> None:
     """Test to check no empty strings are present in the list of gpu's
     when read from files"""
     gpu_list = read_gpu_from_files(filename)
@@ -15,75 +14,95 @@ def test_no_empty_str_read_gpu_from_files(
 
 
 def test_add_gpu_unit_name_default(
-        master_df_test:pd.DataFrame,
-        radeon_gpu_list:list[str],
-        geforce_gpu_list:list[str],
-        intel_gpu_list:list[str],
-        gpu_unit_index:dict[str, dict[str, list[int]]]
+    master_df_test: pd.DataFrame,
+    radeon_gpu_list: list[str],
+    geforce_gpu_list: list[str],
+    intel_gpu_list: list[str],
+    gpu_unit_index: dict[str, dict[str, list[int]]],
 ) -> None:
-    """Test the default behavior of add_gpu_unit - checks if it can 
+    """Test the default behavior of add_gpu_unit - checks if it can
     identify the gpu units from gpu names (according to the behavior of the
     first iteration of gpu4bdgamers)"""
-    # need to specify index, else reading all rows and assigning the 
+    # need to specify index, else reading all rows and assigning the
     # gpu_unit_name to nan
     master_df_test_with_space = master_df_test[:18]
 
-    radeon_df = add_gpu_unit_name(master_df_test_with_space,radeon_gpu_list,"Radeon")
-    geforce_df = add_gpu_unit_name(master_df_test_with_space,geforce_gpu_list,"Geforce")
-    intel_df = add_gpu_unit_name(master_df_test_with_space,intel_gpu_list,"Intel")
+    radeon_df = add_gpu_unit_name(master_df_test_with_space, radeon_gpu_list, "Radeon")
+    geforce_df = add_gpu_unit_name(
+        master_df_test_with_space, geforce_gpu_list, "Geforce"
+    )
+    intel_df = add_gpu_unit_name(master_df_test_with_space, intel_gpu_list, "Intel")
 
-    for gpu_unit_name,index_list in gpu_unit_index['Radeon'].items():
+    for gpu_unit_name, index_list in gpu_unit_index["Radeon"].items():
         for i in index_list:
-            gpu_name:str = master_df_test_with_space.gpu_name.iloc[i]
-            assert radeon_df['gpu_unit_name'][
-                radeon_df['gpu_name']==gpu_name
-            ].iloc[0] == gpu_unit_name
-    
-    for gpu_unit_name,index_list in gpu_unit_index['Geforce'].items():
+            gpu_name: str = master_df_test_with_space.gpu_name.iloc[i]
+            assert (
+                radeon_df["gpu_unit_name"][radeon_df["gpu_name"] == gpu_name].iloc[0]
+                == gpu_unit_name
+            )
+
+    for gpu_unit_name, index_list in gpu_unit_index["Geforce"].items():
         for i in index_list:
-            gpu_name:str = master_df_test_with_space.gpu_name.iloc[i]
-            assert geforce_df['gpu_unit_name'][
-                geforce_df['gpu_name']==gpu_name
-            ].iloc[0] == gpu_unit_name
-    
-    for gpu_unit_name,index_list in gpu_unit_index['Intel'].items():
+            gpu_name: str = master_df_test_with_space.gpu_name.iloc[i]
+            assert (
+                geforce_df["gpu_unit_name"][geforce_df["gpu_name"] == gpu_name].iloc[0]
+                == gpu_unit_name
+            )
+
+    for gpu_unit_name, index_list in gpu_unit_index["Intel"].items():
         for i in index_list:
-            gpu_name:str = master_df_test_with_space.gpu_name.iloc[i]
-            assert intel_df['gpu_unit_name'][
-                intel_df['gpu_name']==gpu_name
-            ].iloc[0] == gpu_unit_name
+            gpu_name: str = master_df_test_with_space.gpu_name.iloc[i]
+            assert (
+                intel_df["gpu_unit_name"][intel_df["gpu_name"] == gpu_name].iloc[0]
+                == gpu_unit_name
+            )
+
 
 def test_add_gpu_unit_name_gpu_name_nospace(
-        master_df_test:pd.DataFrame,
-        radeon_gpu_list:list[str],
-        geforce_gpu_list:list[str],
-        intel_gpu_list:list[str],
+    master_df_test: pd.DataFrame,
+    radeon_gpu_list: list[str],
+    geforce_gpu_list: list[str],
+    intel_gpu_list: list[str],
 ) -> None:
     """Test to check add_gpu_unit_name can correctly identify gpu units
     from gpu names, within which the gpu unit part has no space within it
     e.g. RTX2060"""
     master_df_test_nospace = master_df_test.iloc[18:26]
-    
+
     radeon_df_nospace = add_gpu_unit_name(
-        master_df_test_nospace,radeon_gpu_list,"Radeon"
+        master_df_test_nospace, radeon_gpu_list, "Radeon"
     )
     geforce_df_nospace = add_gpu_unit_name(
-        master_df_test_nospace,geforce_gpu_list,"Geforce"
+        master_df_test_nospace, geforce_gpu_list, "Geforce"
     )
     intel_df_nospace = add_gpu_unit_name(
-        master_df_test_nospace,intel_gpu_list,"Intel"
+        master_df_test_nospace, intel_gpu_list, "Intel"
     )
 
-    radeon_gpu_series = ["Radeon RX 580","Radeon RX 7600","Radeon RX 7600 XT"]
-    geforce_gpu_series = ["Geforce RTX 2060","Geforce RTX 4060","Geforce RTX 4060 Ti"]
-    intel_gpu_series = ["Intel Arc A750","Intel Arc A770"]
+    radeon_gpu_series = ["Radeon RX 580", "Radeon RX 7600", "Radeon RX 7600 XT"]
+    geforce_gpu_series = ["Geforce RTX 2060", "Geforce RTX 4060", "Geforce RTX 4060 Ti"]
+    intel_gpu_series = ["Intel Arc A750", "Intel Arc A770"]
 
     for i in range(3):
 
-        assert radeon_df_nospace['gpu_unit_name'].iloc[i] == radeon_gpu_series[i]
-        
-        assert geforce_df_nospace['gpu_unit_name'].iloc[i] == geforce_gpu_series[i]
-        
-        if i<=1:
-            assert intel_df_nospace['gpu_unit_name'].iloc[i] == intel_gpu_series[i]
+        assert radeon_df_nospace["gpu_unit_name"].iloc[i] == radeon_gpu_series[i]
 
+        assert geforce_df_nospace["gpu_unit_name"].iloc[i] == geforce_gpu_series[i]
+
+        if i <= 1:
+            assert intel_df_nospace["gpu_unit_name"].iloc[i] == intel_gpu_series[i]
+
+
+def test_add_gpu_unit_name_1050_ti() -> None:
+    """Test to observe if GTX 1050 Ti can be detected using add_gpu_unit_name"""
+    master_df = pd.DataFrame(
+        data={
+            "gpu_name": ["Zotac Twin Edge Geforce GTX 1050 Ti", "Uncles GTX 1050Ti"],
+            "gpu_price": [13000, 14000],
+            "retail_url": ["https://weridass.com/zotac-te-1050-ti", "https://uncle.com/1050-ti"]
+        }
+    )
+    df_gpu_unit_name_added = add_gpu_unit_name(
+        target_df=master_df, gpu_list=["GTX 1050 Ti"], gpu_brand="Geforce"
+    )
+    assert df_gpu_unit_name_added["gpu_unit_name"].iloc[0] == "Geforce GTX 1050 Ti"
